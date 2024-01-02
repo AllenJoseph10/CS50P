fd4084710ff0579efe472e7eea974457bab21aa1
Allen Jen Joseph <118963252+AllenJoseph10@users.noreply.github.com>
2024-01-02 22:03:10 +0400

    Create compare.py

---
+++ Lectures/Lecture0/compare.py
@@ -0,0 +1,29 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# if
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
+
+#elif
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+
+#else
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
