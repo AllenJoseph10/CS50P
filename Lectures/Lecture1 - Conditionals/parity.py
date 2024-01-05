35689075a61262bf1f11d3ec274366b94e6e9689
Allen Jen Joseph <118963252+AllenJoseph10@users.noreply.github.com>
2024-01-05 15:34:09 +0400

    Update parity.py

--- Lectures/Lecture1 - Conditionals/parity.py
+++ Lectures/Lecture1 - Conditionals/parity.py
@@ -19,3 +19,25 @@ def is_even(n):
         return False
 
 main()
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even ")
+    else:
+        print("Odd ")
+
+def is_even(n):
+    return True if n % 2 == 0 else False
+
+main()
+
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even ")
+    else:
+        print("Odd ")
+
+def is_even(n):
+    return n % 2 == 0
