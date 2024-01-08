af25d1bbdd74f475c28041c927cea055b39b273c
Allen Jen Joseph <118963252+AllenJoseph10@users.noreply.github.com>
2024-01-08 10:36:00 +0400

    Create compare.py

---
+++ Lectures/Lecture 1 - Conditionals/compare.py
@@ -0,0 +1,50 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# if
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
+
+#elif
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+
+#else
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
+
+#or
+
+if x < y or x > y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+# not equal
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
+
+#equal
+
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
